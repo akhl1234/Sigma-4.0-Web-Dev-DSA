6c57e5a0957903352d7cbb388650dab9944710a1
akhl1234 <shameer.alam2512@outlook.com>
2024-09-13 20:52:41 +0530

    Display user's name and age

---
+++ Javascript/Part-2/Q2.js
@@ -0,0 +1,7 @@
+// Qs2: Take the user's name & age as input using prompts.
+// Then return back the following statement to the user as an alert (by substituting their name & age):
+// "name is age years old." [Use template literals to print this sentence]
+
+let name = prompt("Enter your name:");
+let age = prompt("Enter your age:");
+alert(`${name} is ${age} years old.`);
