6f42c9d461e3e42c1783963b586d91bd722c465f
akhl1234 <shameer.alam2512@outlook.com>
2024-09-13 20:55:03 +0530

    Find the largest of three numbers

---
+++ Javascript/Part-2/Q5.js
@@ -0,0 +1,7 @@
+// Qs5: Write a program to find the largest of 3 numbers.
+
+let num1 = 10,
+  num2 = 20,
+  num3 = 15; // Example numbers
+let largest = Math.max(num1, num2, num3);
+console.log(`The largest number is ${largest}`);
