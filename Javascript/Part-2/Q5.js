83f5552ab0284794eda01333c17411f587cae7dc
akhl1234 <shameer.alam2512@outlook.com>
2024-09-18 22:35:59 +0530

    fix minor

--- Javascript/Part-2/Q5.js
+++ Javascript/Part-2/Q5.js
@@ -2,6 +2,6 @@
 
 let num1 = 10,
   num2 = 20,
-  num3 = 15; // Example numbers
+  num3 = 14; // Example numbers
 let largest = Math.max(num1, num2, num3);
 console.log(`The largest number is ${largest}`);
