c01864da6ad9d226fef5f6e0ab868ec8df6ffbbe
akhl1234 <shameer.alam2512@outlook.com>
2024-09-29 11:42:14 +0530

    Added vowel counting function

---
+++ Javascript/Part-6/Q4.js
@@ -0,0 +1,14 @@
+// Count the number of vowels in a string
+function countVowels(str) {
+  let vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+}
+
+let inputString = "Hello World";
+console.log(countVowels(inputString)); // Output: 3
