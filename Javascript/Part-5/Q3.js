105fb93f2865660be5b665ecc89dc026d95d8e13
akhl1234 <shameer.alam2512@outlook.com>
2024-09-29 11:34:27 +0530

    Change Obj Peoperty

---
+++ Javascript/Part-5/Q3.js
@@ -0,0 +1,11 @@
+let person = {
+  name: "John Doe",
+  age: 30,
+  city: "Los Angeles",
+};
+
+// Changing the city and adding a new property
+person.city = "New York";
+person.country = "United States";
+
+console.log(person);
