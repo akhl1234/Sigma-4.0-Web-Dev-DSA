e8ad9398b7bd6aacbf2701455ca6dfb62b47d1cf
Akhlaque Ansari <124654189+akhl1234@users.noreply.github.com>
2025-03-26 05:26:52 +0000

    Added h1 with underlined "DOM Practice"

---
+++ Javascript/Part-9/Q4.js
@@ -0,0 +1,8 @@
+// Creating h1 element and styling
+const heading = document.createElement('h1');
+heading.textContent = 'DOM Practice';
+heading.style.textDecoration = 'underline';
+heading.style.color = 'purple';
+
+// Appending to body
+document.body.appendChild(heading);
